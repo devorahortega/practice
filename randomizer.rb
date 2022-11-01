6cb71c502cf1f46c19f927752bbbaa7748350dfd
Devorah Ortega <devorahb.ortega@gmail.com>
2022-11-01 12:25:33 -0500

    Adds randomizers

--- randomizer.rb
+++ randomizer.rb
@@ -11,3 +11,11 @@ p SecureRandom.hex
 p SecureRandom.alphanumeric
 
 p [5, 15, 30, 60].shuffle.first
+
+p [5, 15, 30, 60].sample
+
+p ("a".."z").to_a.sample
+
+p [1, 2, 3].sample(2)
+
+[1, 2, 3].sample(random: SecureRandom)
